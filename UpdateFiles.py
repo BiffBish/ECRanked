3175ba661034ab55ae7d0f1c0ed17c8a2837be98
BiffBish <42410516+BiffBish@users.noreply.github.com>
2021-08-16 08:59:18 -0400

    Update UpdateFiles.py

--- UpdateFiles.py
+++ UpdateFiles.py
@@ -2,10 +2,6 @@
 
 import os
 
-old_file_name = "/home/career_karma/raw_data.csv"
-new_file_name = "/home/career_karma/old_data.csv"
-
-
 maps = ["combustion","dyson","fission","surge"]
 for map in maps:
     mapPath = f"D:/Replays/{map}"
@@ -15,3 +11,4 @@ for map in maps:
         os.rename(f"{mapPath}/{f}", f"{mapPath}/{f[:-10]}.ecreplay")
 
 print("Files renamed!")
+
